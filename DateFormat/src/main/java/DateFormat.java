8074f2fcb465b3e1ee20da38606f39178670da8f
othertwink <dbaev9@gmail.com>
2024-09-11 21:33:59 +0300

    Jackson dependency

--- DateFormat/src/main/java/DateFormat.java
+++ DateFormat/src/main/java/DateFormat.java
@@ -1,10 +1,11 @@
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 // @Getter из Lombok
 public class DateFormat {
-
-    // spring
-    // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy:MM:dd##:HH:mm:ss:SSS", locale = "ru")
+    
+     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy:MM:dd##:HH:mm:ss:SSS", locale = "ru")
     private LocalDateTime dateTime;
 
     public DateFormat(LocalDateTime dateTime) {
